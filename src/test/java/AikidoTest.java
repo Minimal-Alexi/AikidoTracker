dcc4a4917c0534066f3e27426ca2871b2e2d94a4
Minimal-Alexi <alexpop@metropolia.fi>
2025-02-27 14:54:30 +0200

    Preparing jenkins.

--- src/test/java/AikidoTest.java
+++ src/test/java/AikidoTest.java
@@ -1,3 +1,19 @@
-public class AikidoTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AikidoTest {
+    private Aikido aikido;
+    @BeforeEach
+    public void setUp() {
+        aikido = new Aikido();
+    }
+    @Test
+    void testAddPracticeSession() {
+        aikido.addSession(new Date(), 90);
+        assertEquals(90, aikido.getTotalPracticeTime());
+    }
 }
