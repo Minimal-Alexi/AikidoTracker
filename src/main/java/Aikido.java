4b4df5c6768a667c2d9c49cd0f942be65a7b8539
Minimal-Alexi <alexpop@metropolia.fi>
2025-02-27 13:52:00 +0200

    Setting up all the needed methods.

--- src/main/java/Aikido.java
+++ src/main/java/Aikido.java
@@ -1,9 +1,29 @@
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Aikido {
+    private int totalTime;
+    private HashMap<Date,Integer> logOfSessions;
+    public Aikido() {
+        totalTime = 0;
+        logOfSessions = new HashMap<>();
+    }
+    public void addSession(Date date, int sessionTime) {
+        logOfSessions.put(date,sessionTime);
+        totalTime += sessionTime;
+    }
+    public boolean checkEligibility(){
+
+    }
+
     public static void main(String[] args) {
+        // Initialization
+        Aikido aikido = new Aikido();
         boolean run = true;
         Scanner scanner = new Scanner(System.in);
+
+        // Runtime
         while(run) {
             System.out.println("1 - Add Practice Session");
             System.out.println("2 - View Total Practice Time");
