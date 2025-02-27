3bdfeda0254ce10b5691c8dd5d6528f0744f6427
Minimal-Alexi <alexpop@metropolia.fi>
2025-02-27 13:18:57 +0200

    Adding starter code.

--- src/main/java/Aikido.java
+++ src/main/java/Aikido.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Aikido {
+    public static void main(String[] args) {
+        boolean run = true;
+        while(run) {
+            System.out.println("1 - Add Practice Session");
+            System.out.println("2 - View Total Practice Time");
+            System.out.println("3 - Check Graduation Eligibility");
+            System.out.println("4 - Exit");
+            System.out.println("Enter your choice: ");
+        }
+    }
 }
