63d85e8cd0b0997aabb62fcf873c4c73ffaac2d3
Minimal-Alexi <alexpop@metropolia.fi>
2025-02-27 13:46:38 +0200

    Fixed setup bug.

--- src/main/java/Aikido.java
+++ src/main/java/Aikido.java
@@ -1,12 +1,38 @@
+import java.util.Scanner;
+
 public class Aikido {
     public static void main(String[] args) {
         boolean run = true;
+        Scanner scanner = new Scanner(System.in);
         while(run) {
             System.out.println("1 - Add Practice Session");
             System.out.println("2 - View Total Practice Time");
             System.out.println("3 - Check Graduation Eligibility");
             System.out.println("4 - Exit");
             System.out.println("Enter your choice: ");
+            int option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                {
+
+                }
+                case 2:
+                {
+
+                }
+                case 3:
+                {
+
+                }
+                case 4:
+                {
+
+                }
+                default:
+                {
+                    System.out.println("Wrong choice. Try again.");
+                }
+            }
         }
     }
 }
